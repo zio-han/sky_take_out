8ccad5e27f1d46ce392990ff0f8beba77d25cb1c
zio-han <2696483958@qq.com>
2026-07-06 14:42:11 +0800

    初始化苍穹外卖项目

---
+++ sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -0,0 +1,18 @@
+package com.sky.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "sky.wechat")
+@Data
+public class WeChatProperties {
+    private String appid;
+    private String mchid;
+    private String mchSerialNo;
+    private String privateKeyFilePath;
+    private String weChatPayCertFilePath;
+    private String notifyUrl;
+    private String refundNotifyUrl;
+}
